5161c35d74b4a99b240ded50f78575aec9122966
Aldo Hernandez <aldo.flores.hrz@gmail.com>
2024-11-26 15:58:25 -0600

    Imagenes

--- clasificador.py
+++ clasificador.py
@@ -1,6 +1,11 @@
+import numpy as np
 class Classifier:
-    def __init__(self, char1, char2):
-        self.char1 = char1
-        self.char2 = char2
-
-    def  
+    def __init__(self, attributes:np.array):
+        self.attributes = attributes
+        self.centroids = np.zeros(self.attributes.shape)
+    #def  predict(self, )
+        
+if __name__ == '__main__':
+    array = np.array([[[[[[[[[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],[[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]]]]]]]])
+    print(np.shape(array))
+    print(array[0][0][0][0][0][0][0][0][8])
